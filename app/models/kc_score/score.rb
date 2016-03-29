98b8297bbfac6b10a40bc2961fba07a9ce1eb62f
destinyd <destinyd_war@163.com>
2016-03-29 15:36:56 +0800

    多态关联改用前缀

--- app/models/kc_score/score.rb
+++ app/models/kc_score/score.rb
@@ -6,8 +6,8 @@ module KcScore
     field :score, :type => Integer
     field :text, :type => String
 
-    belongs_to :sourceable, polymorphic: true
-    belongs_to :targetable, polymorphic: true
+    belongs_to :score_sourceable, polymorphic: true
+    belongs_to :score_targetable, polymorphic: true
 
     validates :score, presence: true
   end
--- sample/spec/models/kc_score/score_spec.rb
+++ sample/spec/models/kc_score/score_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe KcScore::Score, type: :model do
     expect(@score.respond_to?(:score)).to be true
     expect(@score.respond_to?(:text)).to be true
 
-    expect(@score.respond_to?(:sourceable_id)).to be true
-    expect(@score.respond_to?(:sourceable_type)).to be true
-    expect(@score.respond_to?(:targetable_id)).to be true
-    expect(@score.respond_to?(:targetable_type)).to be true
+    expect(@score.respond_to?(:score_sourceable_id)).to be true
+    expect(@score.respond_to?(:score_sourceable_type)).to be true
+    expect(@score.respond_to?(:score_targetable_id)).to be true
+    expect(@score.respond_to?(:score_targetable_type)).to be true
   end
 
   it "关系" do
     @score = create(:score)
-    expect(@score.respond_to?(:sourceable)).to be true
-    expect(@score.respond_to?(:targetable)).to be true
+    expect(@score.respond_to?(:score_sourceable)).to be true
+    expect(@score.respond_to?(:score_targetable)).to be true
   end
 
   #describe "scopes" do
